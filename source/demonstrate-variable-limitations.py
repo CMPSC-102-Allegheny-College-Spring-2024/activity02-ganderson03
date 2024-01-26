73f906baa52406fd01901c954d4a31ef716aed7d
ganderson03 <anderson03@allegheny.edu>
2024-01-26 10:29:50 -0500

    uncommented lines in d-v-l file

--- source/demonstrate-variable-limitations.py
+++ source/demonstrate-variable-limitations.py
@@ -9,9 +9,10 @@ another_feasible_number = 2**2**10
 print(f"The value of another feasible number is {another_feasible_number}")
 print()
 
+
+less_feasible_number = 2**2**100
+print(f"The value of a less feasible number is {less_feasible_number}")
+print()
+
 # NOTE: Uncomment this line and run it!
 # NOTE: Make sure that this line is commented-out when run in GitHub Actions
-
-# less_feasible_number = 2**2**100
-# print(f"The value of a less feasible number is {less_feasible_number}")
-# print()
